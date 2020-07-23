4c6cbdcc978692654e5c65f14a3a68d6ceab0656
Jens M. Glattetre <Jens@superoffice.com>
2020-07-23 13:07:03 +0200

    Adding web project

---
+++ Source/Glattetre.Covid19Data.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Glattetre.Covid19Data.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
